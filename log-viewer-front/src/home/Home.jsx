16daefeb032c94415604d8f6c4358870add7050a
Aaron Carrasco <enriqueaaron7@gmail.com>
2024-01-10 15:58:23 -0500

    Create initial code of the frontend to be used

--- log-viewer-front/src/home/Home.jsx
+++ log-viewer-front/src/home/Home.jsx
@@ -1,32 +1,41 @@
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [token, setToken] = useState("")
+  const [owner, setOwner] = useState("eacarras")
+  const [repository, setRepository] = useState("git_log_viewer")
+
   return (
-    <div className="App">
+    <div className="flex flex-col p-6">
       <header className="App-header">
-        <h1 className="text-3xl font-bold ">Git Log Viewer App</h1>
+        <h1 className="text-3xl font-bold">Git Log Viewer App</h1>
       </header>
       
-      <aside>
+      <aside className='flex flex-col'>
         {/* Section of inputs */}
-        <div>
-          <div>
-            <p>Owner</p>
-            <input type="text" />
+        <h3 className='text-xl font-bold mt-6'>Custom forms</h3>
+        <div className='flex flex-col gap-3'>
+          <div className='flex flex-row gap-2 items-center'>
+            <p className='w-40 text-base'>Owner</p>
+            <input className='flex-1 border border-black rounded-md p-1' type="text" value={owner} />
           </div>
 
-          <div>
-            <p>Repository name</p>
-            <input type="text" />
+          <div className='flex flex-row gap-2 items-center'>
+            <p className='w-40 text-base'>Repository name</p>
+            <input className='flex-1 border border-black rounded-md p-1' type="text" value={repository} />
           </div>
 
-          <div>
-            <p>Token (if apply)</p>
-            <input type="text" />
+          <div className='flex flex-row gap-2 items-center'>
+            <p className='w-40 text-base'>Token (if apply)</p>
+            <input className='flex-1 border border-black rounded-md p-1' type="text" value={token} />
           </div>
         </div>
 
         {/* Viewer logs */}
-        <div>Viewer log</div>
+        <div className='mt-16'>
+          Viewer log
+        </div>
       </aside>
     </div>
   );
